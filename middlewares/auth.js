705d87253cbf92b739d1b8624b816adde86d946a
RT503 <musinspiration@yandex.ru>
2021-06-26 05:57:04 +0300

    hotfix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,21 +4,11 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 const JWT_SECRET = 'fkawflawfoisadfl241';
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация!');
-  }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    const payload = jwt.verify(req.cookies.jwt, JWT_SECRET);
+    req.user = payload;
+    next();
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация!');
   }
-  req.user = payload;
-
-  next();
 };
